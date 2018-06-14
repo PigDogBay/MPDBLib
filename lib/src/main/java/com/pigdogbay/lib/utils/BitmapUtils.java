189d2422bb8488075b02486d4e199c13657dea56
mark <mpdbailey.technology@gmail.com>
2018-06-14 09:55:03 +0100

    New Bitmap utils

---
+++ lib/src/main/java/com/pigdogbay/lib/utils/BitmapUtils.java
@@ -0,0 +1,43 @@
+package com.pigdogbay.lib.utils;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.VectorDrawable;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.content.ContextCompat;
+
+public final class BitmapUtils {
+    private static Bitmap getBitmapFromVectorDrawable(Drawable drawable) {
+        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(),
+                drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+
+        return bitmap;
+    }
+
+    /**
+     * Convert drawable (image or vector) to Bitmap
+     * https://stackoverflow.com/questions/33696488/getting-bitmap-from-vector-drawable
+     *
+     * @param context activity
+     * @param drawableId resource id of drawable
+     * @return Bitmap
+     */
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public static Bitmap getBitmap(Context context, int drawableId) {
+        Drawable drawable = ContextCompat.getDrawable(context, drawableId);
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable) drawable).getBitmap();
+        } else if (drawable instanceof VectorDrawable) {
+            return getBitmapFromVectorDrawable(drawable);
+        } else {
+            throw new IllegalArgumentException("unsupported drawable type");
+        }
+    }
+}
